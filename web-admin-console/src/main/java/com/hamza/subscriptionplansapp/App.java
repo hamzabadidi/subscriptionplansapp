f7fd4d99b32c52fab8f6dec2afa73960a18351b2
Hamza <hamza.badidi@outlook.com>
2015-08-14 13:51:22 +0200

    Initial commit: Maven project structure

---
+++ web-admin-console/src/main/java/com/hamza/subscriptionplansapp/App.java
@@ -0,0 +1,13 @@
+package com.hamza.subscriptionplansapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
